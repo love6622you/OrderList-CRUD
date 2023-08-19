31593af5a3a9bfe7c86bd6d15143bd87eb73ae6c
Chia <lovemaple0913@gmail.com>
2023-08-20 01:14:44 +0800

    add: price field only input number

--- src/model/formSchema.ts
+++ src/model/formSchema.ts
@@ -2,7 +2,10 @@ import z from "zod";
 
 export const orderFormSchema = z.object({
   name: z.string().nonempty({ message: "品項名稱必填" }),
-  price: z.string().nonempty({ message: "價格必填" }),
+  price: z
+    .string()
+    .nonempty({ message: "價格必填" })
+    .regex(/^[0-9]*$/, "限制輸入輸字"),
   size: z.string({
     required_error: "尺寸必填"
   }),
